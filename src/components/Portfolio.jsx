4b1b9638707f4013a40417adde0a3c88d8a1f6c7
Matthew-Graves-Git <matthew.graves.cs@gmail.com>
2023-07-31 19:37:03 -0400

    added portfolio section barebones

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,3 +1,8 @@
+import sorting from '../assets/sorting.png';
+import resourcify from '../assets/resourcify1.png';
+import portfolioImage from '../assets/portfolio.png';
+
+
 const Portfolio = () => {
     return (
         <div
@@ -10,21 +15,21 @@ const Portfolio = () => {
                 Portfolio
               </p>
             </div>
+            <div className='md:w-full w-[95%] h-full grid grid-cols-1 justify-center items-center align-middle md:grid-cols-2 gap-6 py-6 px-12 sm:px-0'>
+              <img className='border-cyan-500 border-2'src={resourcify} alt="" />
+              <img className='border-gray-500 border-2' src={sorting} alt="" />
+              <img  className='border-gray-500 border-2' src={portfolioImage} alt="" />
+                    {/* {techs.map(({src,title,style},index)=>{
+                    return(
+                    <div
+                     key={index}
+                     className={`fade-left shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                            <img src={src} alt={title} className='w-20 mx-auto' />
+                            <p className='mt-4'>{title}</p>
+                    </div>
+                    )})} */}
+            </div>
     
-            <p className="fade-left text-xl mt-20">
-            My name is Matthew Graves, I am a full stack software developer, and am currently
-            finishing my degree in computer science at Oakland University. 
-            Throughout the past few years, I have been dedicated to learning 
-            and mastering various web technologies. This plethora of experience will allow
-             me to provide value to any company.
-            </p>
-    
-            <br />
-    
-            <p className="fade-left text-xl">
-            I pride myself in my attention to detail and focus on creating clean, stable and efficient
-             software solutions with a focus on responsive design, accessibility, and performance.
-            </p>
           </div>
         </div>
       );
